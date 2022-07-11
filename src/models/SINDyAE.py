ac81e7ebf649798b5f6d4dbf3adf2a94a2f3867d
mozesjacobs <mozesj@cs.washington.edu>
2022-07-11 10:56:23 -0700

    allows for different nonlinearities in the encoder and decoder

--- src/models/SINDyAE.py
+++ src/models/SINDyAE.py
@@ -16,22 +16,10 @@ class Net(nn.Module):
         self.include_constant = args.include_constant
         self.library_dim = library_size(self.z_dim, self.poly_order, use_sine=self.use_sine, include_constant=self.include_constant)
         self.mse = nn.MSELoss(reduction='mean')
+        self.nonlinearity = args.nonlinearity
         
-        self.encoder = nn.Sequential(
-            nn.Linear(self.u_dim, self.hidden_dim),
-            nn.Sigmoid(),
-            nn.Linear(self.hidden_dim, self.hidden_dim),
-            nn.Sigmoid(),
-            nn.Linear(self.hidden_dim, self.z_dim)
-        )
-        
-        self.decoder = nn.Sequential(
-            nn.Linear(self.z_dim, self.hidden_dim),
-            nn.Sigmoid(),
-            nn.Linear(self.hidden_dim, self.hidden_dim),
-            nn.Sigmoid(),
-            nn.Linear(self.hidden_dim, self.u_dim)
-        )
+        self.encoder = self.build_net(self.u_dim, self.hidden_dim, self.z_dim)
+        self.decoder = self.build_net(self.z_dim, self.hidden_dim, self.u_dim)
         
         self.sindy_coefficients = nn.Parameter(torch.randn(self.library_dim, self.z_dim, requires_grad=True))
         nn.init.xavier_normal_(self.sindy_coefficients)
@@ -76,14 +64,29 @@ class Net(nn.Module):
         dz = dL
         for i in range(len(net) - 1):
             curr_layer = net[i]
+            # if linear layer, do the matrix multiply and add bias
             if isinstance(curr_layer, nn.Linear):
-                w, b = curr_layer.weight, curr_layer.bias
-                wT = w.T
-                layer_output = torch.sigmoid(torch.matmul(layer_output, wT) + b)
-                d_layer_output = layer_output * (1 - layer_output)  # derivative of sigmoid: σ(x) * (1−σ(x))
-                dz = d_layer_output * (torch.matmul(dz, wT))
-        dz = torch.matmul(dz, net[-1].weight.T)
-        return dz
+                wT, b = curr_layer.weight.T, curr_layer.bias
+                output_before_act = torch.matmul(layer_output, wT) + b
+            else: # if its the activation function, skip to next layer
+                continue
+            # calculate derivative for each type of nonlinearity (or no nonlinearity)
+            if self.nonlinearity == 'sig':
+                # derivative of sigmoid(x): σ(x) * (1−σ(x))
+                layer_output = torch.sigmoid(output_before_act)
+                d_layer_output = layer_output * (1 - layer_output)  
+            elif self.nonlinearity == 'relu':
+                # derivative of relu(x): 1 if x > 0, else 0
+                layer_output = nn.functional.relu(output_before_act)
+                d_layer_output = (output_before_act > 1).float()
+            elif self.nonlinearity == 'elu':
+                # derivative of elu(x): 1 if x > 0, else alpha * exp(x). we use alpha = 1.0 by default
+                layer_output = nn.functional.elu(output_before_act)
+                d_layer_output = torch.min(torch.exp(output_before_act), torch.ones_like(output_before_act))[0]
+            else: # no nonlinearity
+                d_layer_output = 1
+            dz = d_layer_output * torch.matmul(dz, wT)
+        return torch.matmul(dz, net[-1].weight.T)
     
     def loss_func(self, x, x_recon, dx_pred, dz_pred, dx, dz, lambdas):
         # reconstruction loss
@@ -99,3 +102,24 @@ class Net(nn.Module):
         l_reg = lambdas[2] * torch.abs(self.sindy_coefficients).mean()
         
         return l_recon, l_dx, l_dz, l_reg
+
+    def build_net(self, in_dim, hidden_dim, out_dim):
+        if self.nonlinearity == 'elu':
+            nonlinearity = nn.ELU
+        elif self.nonlinearity == 'sig':
+            nonlinearity = nn.Sigmoid
+        elif self.nonlinearity == 'relu':
+            nonlinearity =  nn.ReLU
+        else:  # no nonlinearity 
+            return nn.Sequential(
+                nn.Linear(in_dim, hidden_dim),
+                nn.Linear(hidden_dim, hidden_dim),
+                nn.Linear(hidden_dim, out_dim)
+            )
+        return nn.Sequential(
+            nn.Linear(in_dim, hidden_dim),
+            nonlinearity(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nonlinearity(),
+            nn.Linear(hidden_dim, out_dim)
+        )
